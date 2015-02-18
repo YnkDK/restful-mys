ec355cf313eb48580c190626c967ffe9429a9dfb
Martin Storgaard <20140000@post.au.dk>
2015-02-18 14:24:51 +0100

    Updated test, now testing headers aswell

--- test/hello.py
+++ test/hello.py
@@ -1,10 +1,15 @@
 import urllib2
 import json
 
-data = json.load(urllib2.urlopen('http://localhost:5000/'))  
-if 'message' in data and data['message'] == 'Hello World!':
-	print "Successfully got: '{:s}'".format(data['message'])
-	
+response = urllib2.urlopen('http://localhost:5000/')
+
+assert response.code == 200
+assert response.info()['connection'] == 'close'
+assert response.info()['pragma'] == 'no-cache'
+assert response.info()['cache-control'] == 'no-cache, no-store, max-age=0'
+assert response.info()['content-type'] == 'application/json; charset=utf-8'
+print 'Headers are as expected, content:\n{:s}'.format(response.read())
+
 data = json.load(urllib2.urlopen('http://localhost:5000/?name=MYS'))  
 if 'message' in data and data['message'] == 'Hello MYS!':
 	print "Successfully got: '{:s}'".format(data['message'])
