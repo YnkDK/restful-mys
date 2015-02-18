7763e188190abe7f2a409e5773d7173535154f10
Martin Storgaard <20140000@post.au.dk>
2015-02-18 14:05:57 +0100

    Now using the database for getting random names

--- api/controller/hello_world.py
+++ api/controller/hello_world.py
@@ -13,18 +13,22 @@ class HelloWorld(restful.Resource):
 		# Add argument
 		rp.add_argument(
 			name = 'name',
-			default = "World",
+			default = '',
 			required = False,
 			type = str,
-			help = 'Name of the one to greet. Default: World'			
+			help = 'Name of the one to greet. Default: Random unisex name'			
 		)
 		# Parse given arguments
 		self.args = rp.parse_args()
-		
-		self.model = Model()
+		# If the data attribute was set as
+		# a class variable => use it
+		self.model = Model(getattr(self, 'data', None))
 	
 	def get(self):
 		""" Greets the requester
 		"""
-		gretee = self.args['name']
+		if self.args['name'] == '':
+			gretee = self.model.getRandomName()
+		else:
+			gretee = self.args['name']
 		return jsonify({'message': 'Hello {:s}!'.format(gretee)})
