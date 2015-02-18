bea92caa3a3a9abb16f2d54649f450bd8a9520d9
Martin Storgaard <20140000@post.au.dk>
2015-02-18 14:06:07 +0100

    Now using the database for getting random names

--- api/model/hello_world.py
+++ api/model/hello_world.py
@@ -1,3 +1,4 @@
+import random
 
 class HelloWorld(object):
 	""" A passive model structure. If
@@ -6,5 +7,22 @@ class HelloWorld(object):
 	    it have to ask the model. It is
 	    not notified.
 	"""
-	def __init__(self):
-		pass
+	def __init__(self, data):
+		""" Sets the data object given
+		    from the controller. In the
+		    structure used, the data obj
+		    is actually from the core, i.e.
+		    we only create one connection to
+		    the database per request.
+		"""
+		self.data = data
+	
+	def getRandomName(self):
+		# List of all allowed unisex names in Denmark
+		self.data.addCSV('names', 'data/names.csv')
+		# Get all names
+		names = [x for x in self.data.read('names')]
+		# Clean up
+		self.data.removeCSV('names')
+		# Return a random name
+		return random.choice(names)[0]
